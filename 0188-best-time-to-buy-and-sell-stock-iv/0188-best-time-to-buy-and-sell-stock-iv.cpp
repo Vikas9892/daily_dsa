8d42b9d9041af574c08fdb0830999619a55659a1
Vikas Tiwari <vikast4843@gmail.com>
2026-07-14 23:44:48 +0530

    Time: 15 ms (28.81%), Space: 18.4 MB (17%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,50 +1,28 @@
-#include <stack>
-#include <vector>
-#include <utility>
-#include <algorithm>
-#include <numeric>
-using std::stack;
-using std::pair;
-using std::vector;
-using std::nth_element;
-using std::accumulate;
-
 class Solution {
-
 public:
-    int maxProfit(int k, const vector<int>& prices) {
-        vector<int> profits;
-        stack<pair<int, int>> vps;
-        int v = 0;
-        int p = -1;
-        
-        const int n = prices.size();
-        for (;;) {
-            for (v = p + 1; (v + 1) < n && prices[v] >= prices[v + 1]; ++v);
-            for (p = v; (p + 1) < n && prices[p] <= prices[p + 1]; ++p);
-            if (p == v) break;
-            
-            while (!vps.empty() && prices[vps.top().first] >= prices[v]) {
-                auto vp = vps.top();
-                profits.push_back(prices[vp.second] - prices[vp.first]);
-                vps.pop();
-            }
-            while (!vps.empty() && prices[vps.top().second] <= prices[p]) {
-                profits.push_back(prices[vps.top().second] - prices[v]);
-                v = vps.top().first; 
-                vps.pop();
-            }
-            vps.emplace(v, p);
+    int f(int ind, int buy, int cap, vector<int>& prices, int n,
+          vector<vector<vector<int>>>& dp) {
+
+        if (ind == n || cap == 0)
+            return 0;
+
+        if (dp[ind][buy][cap] != -1)
+            return dp[ind][buy][cap];
+
+        if (buy == 1) {
+            return dp[ind][buy][cap] =
+                       max(-prices[ind] + f(ind + 1, 0, cap, prices, n, dp),
+                           0 + f(ind + 1, 1, cap, prices, n, dp));
         }
-        while (!vps.empty()) {
-            auto vp = vps.top(); vps.pop();
-            profits.push_back(prices[vp.second] - prices[vp.first]);
-        }
-        if (k >= profits.size()) {
-            return accumulate(profits.begin(), profits.end(), 0);
-        } else {
-            nth_element(profits.begin(), profits.end() - k, profits.end());
-            return accumulate(profits.end() - k, profits.end(), 0);
-        }
+
+        return dp[ind][buy][cap] =
+                   max(prices[ind] + f(ind + 1, 1, cap - 1, prices, n, dp),
+                       0 + f(ind + 1, 0, cap, prices, n, dp));
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        return f(0, 1, k, prices, prices.size(), dp);
     }
 };
